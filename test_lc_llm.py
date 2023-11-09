ea09a5d9ec89aae8f8cec19143d01be212660bb4
Sudarshan Kamath Barkur <45016072+sudarshan-kamath@users.noreply.github.com>
2023-11-09 10:57:40 +0100

    Update test_lc_llm.py
    Updated demo file to not have server URL stored directly

--- test_lc_llm.py
+++ test_lc_llm.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 if __name__=="__main__":
-    llm = HostedOpenAIChat(uid=os.getenv("DEMO_CLIENT_UID"), server_url="http://localhost:18000", model="gpt-4")
+    llm = HostedOpenAIChat(uid=os.getenv("DEMO_CLIENT_UID"), server_url=os.getenv("SERVER_URL"), model="gpt-4")
     output = llm("Du bist der Assitant. Was ist 2+2?")
     print(output)
