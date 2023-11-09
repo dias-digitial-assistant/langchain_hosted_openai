c81143761710bb5e44a51ef58b435ff2a59ff75a
Sudarshan Kamath Barkur <45016072+sudarshan-kamath@users.noreply.github.com>
2023-11-09 10:58:29 +0100

    Update test_lc_emb.py
    Updated serverurl for embeddings

--- test_lc_emb.py
+++ test_lc_emb.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 if __name__=="__main__":
-    embeddings = HostedOpenAIEmbeddings(uid=os.getenv("DEMO_CLIENT_UID"), server_url="http://localhost:34915")
+    embeddings = HostedOpenAIEmbeddings(uid=os.getenv("DEMO_CLIENT_UID"), server_url= os.getenv("SERVER_URL"))
     text = "This is a test document"
     query_result = embeddings.embed_query(text)
     doc_result = embeddings.embed_documents([text, "This is also a document"])
